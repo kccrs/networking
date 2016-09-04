b8ad933eefb9a23a7d83ec1e2a40e55d23764649
David Kerr <kerrd89@gmail.com>
2016-09-03 18:04:37 -0600

    added webpack and s and selenium successfully

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: "./lib/index.js",
+    test: "mocha!./test/index.js"
+  },
+  output: {
+    path: __dirname,
+    filename: "[name].bundle.js"
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: '/node_modules/', loader: 'babel-loader' },
+      { test: /\.css$/, loader: "style!css" },
+      { test: /\.scss$/, loader: "style!css!sass" }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.json', '.scss', '.css']
+  }
+};
+
+
+// wdio wdio.conf.js
+
+./node_modules/.bin/wdio wdio.conf.js
