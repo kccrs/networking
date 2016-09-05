b493a1fcc33cf5352900b0db76bfb923a1759275
Casey Cross <hellocaseycross@gmail.com>
2016-09-04 18:10:39 -0600

    Pseudo-coded test functions

--- test/user-test.js
+++ test/user-test.js
@@ -6,3 +6,21 @@ describe('our test bundle', function () {
     assert(true);
   });
 });
+
+describe('User constructor', function() {
+
+// it should add a user photo using updateInfo
+// it should add a user photo using updateInfo
+
+// it should add a user firstName using updateInfo
+// it should add a user firstName using updateInfo
+
+// it should add a user lastName using updateInfo
+// it should add a user lastName using updateInfo
+
+
+// it should add a new contact into the array of contacts using addContacts
+
+// it should remove a contact from the array of contacts using deleteContacts
+
+});
