48960657c149baa87ba56b0bef6aec4d8a88656a
mjvalade <mjvalade@gmail.com>
2016-09-05 12:20:14 -0600

    wrote Note unit tests

--- test/note-test.js
+++ test/note-test.js
@@ -1,8 +1,52 @@
+/* globals describe, it, context */
+
 const assert = require('chai').assert;
 const Note = require('../lib/scripts/note');
 
-describe('our test bundle', function () {
-  it('should work', function () {
-    assert(true);
+describe('Note', function () {
+  var note;
+
+  context('constructor', function () {
+
+    it('should default this.content to a value of null', function () {
+      note = new Note();
+
+      assert.equal(note.content, null);
+    });
+
+    it('can receive a different value as an argument', function () {
+      note = new Note('programming');
+
+      assert.equal(note.content, 'programming');
+    });
+
+    it('should default this.time to a number value', function () {
+      note = new Note();
+
+      assert.equal(typeof note.time, 'number');
+    });
   });
+
+  context('updateContent', function () {
+
+    it('should update content of note', function () {
+      note = new Note();
+      var newContent = 'hello again';
+      note.updateContent(newContent);
+
+      assert.equal(note.content, newContent);
+    });
+
+    it('should update time of note', function () {
+      note = new Note();
+      var pastTime = note.time;
+
+      setTimeout(function() {
+        note.updateContent('');
+        assert.notEqual(note.time, pastTime);
+      }, 1000);
+    });
+
+  });
+
 });
