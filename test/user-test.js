7b231ba18035c3342d1aa36daf9af51c207492ec
Casey Cross <hellocaseycross@gmail.com>
2016-09-05 10:53:45 -0600

    Still have two tests failing. Need to havepeer review.

--- test/user-test.js
+++ test/user-test.js
@@ -32,40 +32,41 @@ describe('User constructor', function() {
   });
 
   context('set values for object properties', function() {
-    let firstName = "Bob";
-    // let lastName = "Loblaw"
-    let user = new User({});
-
-    it('should add a users firstName using updateUserInfo', function() {
-      // user.updateUserInfo();
-      assert.equal(user.updateUserInfo(firstName, "Bob"), "Bob");
-    });
+    let userFirstName = "Bob";
+    let userLastName = "Loblaw";
+    let newUser = new User({firstName: userFirstName, lastName: userLastName});
 
     it('should change a users firstName using  updateUserInfo', function() {
-      // let firstName = "Carl";
-      let user = new User({firstName: "Taco"});
-      // let newName = user.firstName;
-      let firstName = "George";
-      user.updateUserInfo(firstName, "George");
-      assert.equal(user.firstName, "George");
+      let newFirstName = "George";
+      newUser.updateUserInfo(newUser.firstName, newFirstName);
+      assert.equal(newUser.firstName, "George");
     });
 
-    it('should add a users lastName using updateUserInfo', function() {
-      user.updateUserInfo();
-      assert.equal(user.lastName, "Johnson");
-    });
-    // it should update a users lastName using updateUserInfo
     it('should update a users lastName using updateUserInfo', function() {
+      let newLastName = "Smith";
+      newUser.updateUserInfo(newUser.lastName, newLastName);
+      assert.equal(newUser.lastName, "Smith");
+    });
+
+    context('contact array functions', function() {
+
+      let anotherUser = new User({});
+
+      it('should add a new contact into the array of contacts using addContacts', function() {
+        anotherUser.addContacts({firstName: "Fancy", lastName: "Pants"});
+        assert.lengthOf(anotherUser.contacts, 1);
+        anotherUser.addContacts({firstName: "Indigo", lastName: "Montoya"});
+        assert.lengthOf(anotherUser.contacts, 2);
+
+      });
+
+      it('should remove a contact from the array of contacts using deleteContacts', function() {
+        anotherUser.deleteContacts(0);
+        assert.lengthOf(anotherUser.contacts, 1);
+      });
 
     });
 
-    it('should add a new contact into the array of contacts using addContacts', function() {
-
-    });
-
-    it('should remove a contact from the array of contacts using deleteContacts', function() {
-
-    });
 
     // TODO:  it should add a users photo using updateUserInfo
 
