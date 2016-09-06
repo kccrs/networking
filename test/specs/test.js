ea4d60cc48fe886a20aeb6d80262bc2f0b88b1b7
David Kerr <kerrd89@gmail.com>
2016-09-06 15:30:16 -0600

    notes now appending to page

--- test/specs/test.js
+++ test/specs/test.js
@@ -2,8 +2,8 @@ const assert =  require('assert');
 
 describe('welcome page', function(){
   it('should be able to grab the page title', function(){
-    browser.url('/');
+    browser.url('/public/index.html');
     var title = browser.getTitle();
-    assert.equal(title, 'Networking');
+    assert.equal(title, 'GetWorking');
   });
 });
