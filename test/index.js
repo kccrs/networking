a277be84e006a7f256a2e7d922e3505f139e4013
David Kerr <kerrd89@gmail.com>
2016-09-06 12:01:55 -0600

    refactored tests to fit same conventions

--- test/index.js
+++ test/index.js
@@ -2,10 +2,3 @@ const assert = require('chai').assert;
 require('./contact-test');
 require('./note-test');
 require('./user-test');
-
-
-describe('our test bundle', function () {
-  it('should work', function () {
-    assert(true);
-  });
-});
