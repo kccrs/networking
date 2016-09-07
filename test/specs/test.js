c806f49fcb0aa01128a6f5d3ea89b414f6347481
Casey Cross <hellocaseycross@gmail.com>
2016-09-06 19:09:40 -0600

    Two tests passing.

--- test/specs/test.js
+++ test/specs/test.js
@@ -1,9 +1,15 @@
 const assert =  require('assert');
 
-describe('welcome page', function(){
+describe('Log in page', function(){
   it('should be able to grab the page title', function(){
     browser.url('/public/index.html');
     var title = browser.getTitle();
     assert.equal(title, 'GetWorking');
+  });
+
+  it('should take you to the home page after clicking log in', function() {
+    browser.url('/public/index.html');
+    browser.click('#log-in-button');
+    assert.equal(browser.getUrl(),'http://localhost:8080/public/views/home.html');
   });
 });
